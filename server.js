37658257ddeaf09affb2106571fa73b339ebe6ac
Mike Peterson <mikemikemike3390@yahoo.com>
2022-07-13 19:36:40 -0700

    heroku push

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://mpeterson56:Champion00@cluster0.onhazic.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pizza-hunt', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
